8a114f5d520204d05a92bf769d8167c52708ba29
Danilov <best-dim@outlook.com>
2023-10-22 09:51:04 +0300

    Add Asp.Net Core Web API

---
+++ APIs/dotnet/DotnetAPI/Models/Employee.cs
@@ -0,0 +1,8 @@
+namespace DotnetAPI.Models;
+
+public class Employee
+{
+    public int Id { get; set;}
+    public string FirstName { get; set;} = "";
+    public string LastName { get; set; } = "";
+}
